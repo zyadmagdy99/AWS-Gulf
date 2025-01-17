0dc1fd7945332b577fd8595ef07541e2a5425413
zyad magdy <zyadmagdy08@gmail.com>
2025-01-17 22:47:55 +0200

    2

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -4,8 +4,8 @@ import { useTheme } from '../context/ThemeContext'
 function Brands() {
   const {Ison} = useTheme();
   return (
-    <div className={`${Ison?"bg-white":"bg-black"}  flex h-[7rem] flex-row-reverse justify-between md:justify-center border shadow-md shadow-gray-200  `}>
-      <div className='flex items-center md:w-1/2 gap-4  md:gap-0 pe-4 md:pe-0 justify-between xl:justify-around'>
+    <div className={`${Ison?"bg-white":"bg-black"}  flex h-[7rem] flex-row-reverse justify-around md:justify-center border shadow-md shadow-gray-200  `}>
+      <div className='flex items-center md:w-1/2 gap-4  md:gap-0 pe-4 md:pe-0 justify-around xl:justify-around'>
         <div className='xl:text-[50px] md:text-[35px] xl:pe-16 font-semibold text-[#009982]'>Kaspersk</div>
         <div className='flex items-center md:ps-10'>
           <p className='xl:text-[40px] md:text-[35px]  text-gray-600 font-semibold'>Microsoft</p>
